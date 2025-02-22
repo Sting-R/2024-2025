e5a780b6efd97ace7a81d5099c246fc2cb327418
Henry Hare <dreadfall64@gmail.com>
2025-02-21 20:33:54 -0600

    Created logging class for the robot

---
+++ 2024-2025/main-bot/src/main/java/frc/robot/Logging.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+import java.lang.Math;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Logging {
+    String Name;
+    boolean isError;
+
+    public Logging(String name) {
+        Name = name;
+    }
+
+    public void log(String message) {
+        System.out.println(Name + ": " + message);
+    }
+
+    public void error(String errorMessage) {
+        isError = true;
+        try {
+            throw new Error("Error:" + Name + ": " + errorMessage);
+        } catch (Error e) {
+            // This does the error thing
+            SmartDashboard.putBoolean(Name + ".Error", true);
+        }
+    }
+
+    public void fixError() {
+        isError = false;
+        SmartDashboard.putBoolean(Name + ".Error", false);
+    }
+}
